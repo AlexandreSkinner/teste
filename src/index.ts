95b4946e01323986a52c9f5d2f3b1f5d9ea761d6
Alexandre Skinner <AlexandreSkinner@tmassessoria.com.br>
2022-09-26 21:23:21 -0300

    feat: implementa roteamento do express dinamico

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@
 // import 'moment/locale/pt-br';
 import { format, add, parse, addDays, isValid, differenceInDays, addMonths, lastDayOfMonth } from 'date-fns';
 import { getDate } from 'date-fns';
+import express, { Request, Response } from "express";
 
 let dataInicio: Date
 let dataTermino: Date
 
 // Cria uma data de inicio
 dataInicio = new Date("01/01/2021")
-console.log( 'Exemplo 01 - Cria uma data de inicio')
+console.log('Exemplo 01 - Cria uma data de inicio')
 console.log(dataInicio)
 
 // Adiciona 1 ano na data informada
@@ -17,7 +18,7 @@ console.log('Exemplo 02 - Adiciona 12 mese a uma data 01/01/2021')
 const result = add(new Date("01/01/2021"), {
     months: 12,
     days: -1,
-  
+
 })
 console.log(result)
 
@@ -26,7 +27,7 @@ console.log(' ')
 console.log('Exemplo 03 - Obtem data anterior a 01/01/2021 12 meses')
 const result2 = add(new Date("01/01/2021"), {
     months: -12,
-  
+
 })
 console.log(result2)
 
@@ -43,7 +44,7 @@ let diaAnterior = -1
 let result3 = add(niver, {
     months: meses,
     days: diaAnterior,
-  
+
 })
 console.log(' ')
 console.log('Exemplo 05 - Soma 120 meses a data 13/06/2021')
@@ -56,7 +57,7 @@ console.log(format(result3, 'dd/MM/yyyy'))
 // Diferença entre duas datas
 console.log(' ')
 console.log('Exemplo 07 - diferença entre datas 01/06/2021 e 30/06/2021')
-let dateLeft =  new Date("06/01/2021")
+let dateLeft = new Date("06/01/2021")
 let dateRight = new Date("06/30/2021")
 let result4 = differenceInDays(dateRight, dateLeft) + 1
 console.log(result4)
@@ -109,10 +110,33 @@ let dataLegal = new Date(dataString)
 console.log(dataLegal)
 
 // Extrair o dia da data
-let diaMes = getDate(dataLegal) +1
+let diaMes = getDate(dataLegal) + 1
 console.log(diaMes)
 
 // Retorna o dia da data 29/01/2012
 console.log(' ');
 console.log('Retorna o dia da data 29/01/2021')
-console.log(getDate(new Date('2012-01-29'))+1)
+console.log(getDate(new Date('2012-01-29')) + 1)
+
+
+// Teste de rotiamento sinamico
+let app: any;
+app = express();
+app.use(express.json());
+
+let method = 'get';
+let url = '/teste';
+
+app[method](url, async function (req: Request, res: Response) {
+    const output = {
+        "metodo": method,
+        "url": url
+    };
+    res.json(output);
+});
+
+app.listen(3000, () => {
+    console.log("🌍 Server started at http://localhost:3000} !!");
+});
+
+
